67c9cd46a57d474b9f04f7e765982d8e4ab78e17
Rbloemhard <raymond_bloemhard@hotmail.com>
2021-02-04 15:24:32 +0100

    saving

--- Basic-opmaak/JavaScript/basic.js
+++ Basic-opmaak/JavaScript/basic.js
@@ -1,62 +1,58 @@
- const toggleMenu = document.getElementById('burger');
+const toggleMenu = document.getElementById('burger');
  toggleMenu.addEventListener('click',function(){
  const sideMenu = document.querySelector('.sidebar');
  sideMenu.classList.toggle('hide-sidebar');
- });
- const colorBody = document.body;
- const colorGray = document.getElementById('item1');
- const colorRed = document.getElementById('item2');
- const colorOrange = document.getElementById('item3');
- const colorPurple = document.getElementById('item4');
- const colorGreen = document.getElementById('item5');
-
- const changeClassBlueBackground = function () {
-    const bodyElement = document.body;
-    bodyElement.classList.toggle("gray-background-color");
-  };
-
-
-
-    colorGray.addEventListener('click', function(){
-        colorGray.classList.add('gray-background-color');
-        const sideMenu = document.querySelector('.sidebar');
-        sideMenu.classList.toggle('hide-sidebar');
-        colorGray.classList.remove('gray-background-color');
-        changeClassBlueBackground();
-    });
-
-    colorRed.addEventListener('click', function(){
-        const bodyColor = document.body;
-        bodyColor.classList.toggle('red-background-color');
-        const sideMenu = document.querySelector('.sidebar');
-        sideMenu.classList.toggle('hide-sidebar');
-        colorRed.classList.remove('red-background-color');
-    })
-    colorOrange.addEventListener('click', function(){
-        const bodyColor = document.body;
-        bodyColor.classList.toggle('orange-background-color');
-        const sideMenu = document.querySelector('.sidebar');
-        sideMenu.classList.toggle('hide-sidebar');
-        colorOrange.classList.remove('Orange-background-color');
-    
-    })
-    colorPurple.addEventListener('click', function(){
-        const bodyColor = document.body;
-        bodyColor.classList.toggle('purple-background-color');
-        const sideMenu = document.querySelector('.sidebar');
-        sideMenu.classList.toggle('hide-sidebar');
-        colorPurple.classList.remove('purple-background-color');
-    
-    })
-    colorGreen.addEventListener('click', function(){
-        const bodyColor = document.body;
-        bodyColor.classList.toggle('green-background-color');
-        const sideMenu = document.querySelector('.sidebar');
-        sideMenu.classList.toggle('hide-sidebar');
-        colorGreen.classList.remove('green-background-color');
-    
-    })
  
+});
+
+const changeColors = () => {
+
+}
+
+
+
+const redBtn = document.getElementById('item2');
+
+redBtn.addEventListener('click', function(){
+  const setBody = document.body;
+  setBody.classList.add('red-background-color');
+  const sideMenu = document.querySelector('.sidebar');
+  sideMenu.classList.toggle('hide-sidebar');
+})
+
+// const redBtn = document.getElementById('item3');
+
+// redBtn.addEventListener('click', function(){
+//   const setBody = document.body;
+//   setBody.classList.add('orange-background-color');
+//   const sideMenu = document.querySelector('.sidebar');
+//   sideMenu.classList.toggle('hide-sidebar');
+// })
+
+// const redBtn = document.getElementById('item4');
+
+// redBtn.addEventListener('click', function(){
+//   const setBody = document.body;
+//   setBody.classList.add('purple-background-color');
+//   const sideMenu = document.querySelector('.sidebar');
+//   sideMenu.classList.toggle('hide-sidebar');
+// })
+
+// const redBtn = document.getElementById('item5');
+
+// redBtn.addEventListener('click', function(){
+//   const setBody = document.body;
+//   setBody.classList.add('green-background-color');
+//   const sideMenu = document.querySelector('.sidebar');
+//   sideMenu.classList.toggle('hide-sidebar');
+// })
+ 
+ 
+
+ 
+
+
+
 
 
 
