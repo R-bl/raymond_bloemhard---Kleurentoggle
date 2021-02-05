2f68451ac055dba935e305debbe20ed27fa83774
Rbloemhard <raymond_bloemhard@hotmail.com>
2021-02-05 20:57:16 +0100

    Fixed color click

--- Main/JavaScript/app.js
+++ Main/JavaScript/app.js
@@ -20,37 +20,39 @@ const orange = document.querySelector('.orange');
 const purple = document.querySelector('.purple');
 const green = document.querySelector('.green');
 
-//make gray
+
 const makeGray = () =>{
-  body.classList.remove('green');
+  body.classList.remove('red','orange','purple','green');
   body.classList.add('gray');
   hideMenu();
 }
 gray.addEventListener('click', makeGray);
 
+
 const makeRed = () =>{
-  body.classList.remove('gray');
+  body.classList.remove('orange','purple','green','gray');
   body.classList.add('red');
   hideMenu();
 }
 red.addEventListener('click', makeRed);
 
+
 const makeOrange = () =>{
-  body.classList.remove('red');
+  body.classList.remove('purple','green','red','gray');
   body.classList.add('orange');
   hideMenu();
 }
 orange.addEventListener('click', makeOrange);
 
 const makePurple = () =>{
-  body.classList.remove('orange');
+  body.classList.remove('green','gray','red','orange');
   body.classList.add('purple');
   hideMenu();
 }
 purple.addEventListener('click', makePurple);
 
 const makeGreen = () =>{
-  body.classList.remove('purple');
+  body.classList.remove('gray','red','orange','purple');
   body.classList.add('green');
   hideMenu();
 }
